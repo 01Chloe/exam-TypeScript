32913b2f044915a36a2026f5bb9405d9236e89d5
01Chloe <allier.chloe@gmaim.com>
2025-09-16 13:15:45 +0200

    partie1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import { greet } from "./utils";
+const select: HTMLSelectElement|null = document.querySelector('#fruitSelect');
+const addBtn: HTMLButtonElement|null = document.querySelector('#addBtn');
+const listContainer: HTMLUListElement|null = document.querySelector('#fruitList');
 
-console.log(greet("Alice"));
+addBtn?.addEventListener('click', getValue);
+function getValue():void{
+    let fruit: string|undefined = select?.value;
+    let item: HTMLLIElement = document.createElement('li');
+    if(fruit){
+        item.textContent = fruit;
+    }
+    listContainer?.insertAdjacentElement("beforeend", item);
+}
